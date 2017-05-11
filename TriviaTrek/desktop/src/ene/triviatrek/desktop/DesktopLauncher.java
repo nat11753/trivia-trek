28cb41dd788f64ca9b01929708d1df0c9ed5dd1c
Eleanor-R <eleanor0918@bell.net>
2017-05-11 13:02:11 -0400

    Add files via upload

---
+++ TriviaTrek/desktop/src/ene/triviatrek/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ene.triviatrek.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ene.triviatrek.TriviaTrek;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TriviaTrek(), config);
+	}
+}
